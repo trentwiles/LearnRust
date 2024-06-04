aad73d49393294e2497cd0c48fb32b5e347693c8
trentwiles <wilest7@gmail.com>
2024-06-03 22:19:55 -0400

    more experimenting

---
+++ notes/cargo.md
@@ -0,0 +1,14 @@
+# Basic Cargo Guide
+
+## Project Commands
+1. New cargo project: `cargo new` (a new Cargo.toml file has been made)
+2. Compile: `cargo build`
+3. Run: `cargo run`
+
+## Crates
+A crate is a package for rust. To add one, you can manually add it to the Cargo.toml file, like so:
+
+```
+[dependencies]
+rand = "0.8.5"
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// i/o library
+use std::io;
+
+// random library, the one we added manually to Cargo.toml
+use rand::Rng;
+
+// main function (kind of like Java)
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // mut = mutable
+    let mut guess = String::new();
+
+    // this is not mutable
+    let example = 0;
+
+    // also not mutable
+    // 1..=100 translates to "1 to 100, inclusive"
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    let intGuess: Result<i32, _> = guess.parse();
+
+    println!("You guessed: {guess}, and that's {}", intGuess==Ok(secret_number));
+    println!("Here's another example of printing something {example}, {}", 2*3)
 }
