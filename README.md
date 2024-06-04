f7e69aefd4a1ebe027bd28b7770eb35b1beacd8a
trentwiles <wilest7@gmail.com>
2024-06-04 11:32:34 -0400

    quick readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Learning Rust
+
+## Left off at:
+[Chapter 3, Part Two](https://doc.rust-lang.org/book/ch03-02-data-types.html)
--- hello.rs
+++
@@ -1,7 +0,0 @@
-
-// How to run this:
-// 1. Compile: rustc main.rs
-// 2. Run binary: ./main
-fn main() {
-    println!("Hello, world!");
-}
