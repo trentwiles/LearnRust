3f271719f18f7d31a8f1898c13f3d3f74ce514cd
trentwiles <wilest7@gmail.com>
2024-06-25 22:01:30 -0400

    start hashmap

---
+++ program_files/hashmap.rs
@@ -0,0 +1,8 @@
+fn main(){
+    use std::collections::HashMap;
+
+    let mut cars = HashMap::new();
+
+    scores.insert(String::from("Volvo"), 30000);
+    scores.insert(String::from("Honda"), 50000);
+}
